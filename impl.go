2bf893cece2c3fbf3f10fe586d573e588ca5b902
Josh Bleecher Snyder <josh@tailscale.com>
2021-07-06 16:46:57 -0700

    improve help text
    particularly when run with -h

--- impl.go
+++ impl.go
@@ -20,20 +20,6 @@ import (
 	"golang.org/x/tools/imports"
 )
 
-const usage = `impl [-dir directory] <recv> <iface>
-
-impl generates method stubs for recv to implement iface.
-
-Examples:
-
-impl 'f *File' io.Reader
-impl Murmur hash.Hash
-impl -dir $GOPATH/src/github.com/josharian/impl Murmur hash.Hash
-
-Don't forget the single quotes around the receiver type
-to prevent shell globbing.
-`
-
 var (
 	flagSrcDir = flag.String("dir", "", "package source directory, useful for vendored code")
 )
@@ -414,11 +400,31 @@ func flattenCommentMap(m ast.CommentMap) string {
 }
 
 func main() {
+	flag.Usage = func() {
+		fmt.Fprint(os.Stderr, `
+impl generates method stubs for recv to implement iface.
+
+impl [-dir directory] <recv> <iface>
+
+`[1:])
+		flag.PrintDefaults()
+		fmt.Fprint(os.Stderr, `
+
+Examples:
+		
+impl 'f *File' io.Reader
+impl Murmur hash.Hash
+impl -dir $GOPATH/src/github.com/josharian/impl Murmur hash.Hash
+
+Don't forget the single quotes around the receiver type
+to prevent shell globbing.
+`[1:])
+		os.Exit(2)
+	}
 	flag.Parse()
 
 	if len(flag.Args()) < 2 {
-		fmt.Fprint(os.Stderr, usage)
-		os.Exit(2)
+		flag.Usage()
 	}
 
 	recv, iface := flag.Arg(0), flag.Arg(1)
